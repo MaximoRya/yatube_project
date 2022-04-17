84107057eb0002977e37df3f6e31c754eb89b6ff
Максим Рябов <maximorya@gmail.com>
2022-04-17 08:04:14 +0400

    Пробую запустить сервер 2

--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
 
 # Create your views here.
+def index(request):
+    return HttpResponse(f'Поехали...')
+
+
+# В урл мы ждем парметр, и нужно его прередать в функцию для использования
+def group_posts(request, num):
+    return HttpResponse(f'Мороженое номер {num}')
+    
